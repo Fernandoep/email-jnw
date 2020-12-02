7bf16d7c210d22f565d640e2a03472782540fb4f
Fernandoep <43655716+Fernandoep@users.noreply.github.com>
2020-12-02 08:34:19 -0300

    upp
    upp

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ var transporter = nodemailer.createTransport({
     secure: false,
     auth: {
         user: 'fernando@fernandoparanhos.com.br',
-        pass: 'aF*232892!@#*&!'
+        pass: 'aF*232892!@#*&!"
    }
 });
 
@@ -28,7 +28,7 @@ app.get('/', (req, res) => res.send('servidor rodando'));
 app.post('/login', function (req, res) {
     console.log('Body ===>', req.body);
     var mailOptions = {
-        from: 'site@fernandoparanhos.com.br',
+        from: 'fernando@fernandoparanhos.com.br',
         to: 'fernandoeparanhos@gmail.com',
         subject: 'Conato JNW',
         text: ` Telefone: ${req.body.telefone}\n Mensagem: ${req.body.conteudo}\n E-mail: ${req.body.email} \n Para realizar sonhos: ${req.body.emailindex}`
